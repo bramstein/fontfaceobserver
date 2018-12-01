969dc35d6362699a41779d38eb6498e45556fba1
Bram Stein <b.l.stein@gmail.com>
2018-12-01 17:55:25 +0100

    Fix calling phantomjs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
       build: ['build']
     },
     exec: {
-      test: 'phantomjs node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js test/index.html',
+      test: './node_modules/.bin/phantomjs node_modules/mocha-phantomjs-core/mocha-phantomjs-core.js test/index.html',
       deps: 'calcdeps -i src -i exports.js -p src -p ./vendor/google/base.js -p node_modules/closure-dom/src/dom.js -o deps > test/deps.js'
     },
     jshint: {
